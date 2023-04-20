fbe0a6ebccb068acb676d6be67fa3da734cb9824
chkrishna142 <chkrishna142@gmail.com>
2023-04-20 16:19:57 +0530

    ade everything

--- signing.js
+++ signing.js
@@ -1,31 +1,32 @@
 // var dataArray = JSON.parse(localStorage.getItem("store")) || [];
-require("dotenv").config();
+
 
 // console.log(process.env.SERVER);
 
+// console.log("hi");
+//console.log(process.env.SERVER);
 
-console.log(process.env.SERVER);
+async function storedata() {
+    event.preventDefault();
+    var objArray = {
+        name: document.getElementById("name").value,
+        phone: document.getElementById("phone").value,
+        email: document.getElementById("email").value,
+        password: document.getElementById("pass").value,
+    };
 
-// async function storedata() {
-//     event.preventDefault();
-//     var objArray = {
-//         name: document.getElementById("name").value,
-//         phone: document.getElementById("phone").value,
-//         email: document.getElementById("email").value,
-//         password: document.getElementById("pass").value,
-//     };
-
-//     await fetch(process.env.SERVER`/signup`, {
-//         method: "POST",
-//         headers: { "Content-Type": "application/json" },
-//         body: JSON.stringify(objArray),
-//     })
-//         .then((res) => res.json())
-//         .then((data) => {
-//             console.log(data);
-//             window.location.href = "login.html";
-//         })
-//         .catch((err) => console.log(err));
+    await fetch(`http://localhost:5001/signup`, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(objArray),
+    })
+        .then((res) => res.json())
+        .then((data) => {
+            console.log(data);
+            alert("sign up successfull")
+            window.location.href = "login.html";
+        })
+        .catch((err) => console.log(err));
 
     // dataArray.push(objArray);
     // localStorage.setItem("store", JSON.stringify(dataArray));
@@ -36,4 +37,4 @@ console.log(process.env.SERVER);
     // document.getElementById("email").value = "";
     // document.getElementById("pass").value = "";
     // document.getElementById("mobile").value = "";
-// }
+}
